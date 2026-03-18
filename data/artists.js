87605ad4013c9900f0cc825b6908d16b10845e69
Gabriel <gabrielsb1090@gmail.com>
2026-03-18 19:08:11 -0300

    Criando dados de artistas

---
+++ data/artists.js
@@ -0,0 +1,22 @@
+const artists = [
+  {
+    id: 1,
+    name: "Drake",
+    genre: "Rap",
+    country: "Canadá",
+  },
+  {
+    id: 2,
+    name: "Travis Scott",
+    genre: "Rap",
+    country: "EUA",
+  },
+  {
+    id: 3,
+    name: "The Weeknd",
+    genre: "R&B",
+    country: "Canadá",
+  }
+];
+
+export default artists;
